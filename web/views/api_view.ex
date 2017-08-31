0fcc98ba31756eea746b5c9b5d6102bc4025225b
Emmett Harper <eharpe23@yahoo.com>
2017-08-30 21:56:48 -0700

    cleaned up the ui

--- web/views/api_view.ex
+++ web/views/api_view.ex
@@ -2,34 +2,12 @@ defmodule Documentr2.ApiView do
   use Documentr2.Web, :view
   alias Documentr2.Path
 
-  # def render("index.html", %{apis: apis}) do
-  #   %{
-  #     apis: Enum.map(apis, &api_json/1)
-  #   }
-  # end
-  #
-  # def api_json(api) do
-  #   %{
-  #     basePath: api.basePath,
-  #     definitions: api.definitions,
-  #     paths: api.paths,
-  #     inserted_at: api.inserted_at,
-  #     updated_at: api.updated_at
-  #   }
-  # end
-  # def render("index.json", %{apis: apis}) do
-  #   render_many(apis, __MODULE__, "api.json")
-  # end
-  #
-  # def render("api.json", %{api: api}) do
-  #   %{
-  #     basePath: api.basePath,
-  #     definitions: api.definitions,
-  #     paths: api.paths
-  #   }
-  # end
   def side_bar_title() do
-    "Available Api Paths: "
+    "Routes"
+  end
+
+  def definition_title() do
+    "Route Definitions"
   end
 
   def get_versions(apis) do
@@ -43,6 +21,10 @@ defmodule Documentr2.ApiView do
 
   def get_method_class(type) do
     "method #{type}"
+  end
+
+  def get_definition_container_class(type) do
+    "#{type}"
   end
 
 end
